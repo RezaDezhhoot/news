8244f332f59c92a3348238a881ffe0c514419082
Reza Dezhhoot <rdezhhoot@gmail.com>
2023-04-07 21:38:19 +0330

    refactor sms service

--- app/Modules/Auth/Services/SmsService.js
+++ app/Modules/Auth/Services/SmsService.js
@@ -1,4 +1,7 @@
 const {send} = require("./SMS/KavehNegarService");
 module.exports.send = async ( receptor , code , template = 'verification') => {
+    if (process.env.MODE === 'test')
+        return 200;
+    
     return await send(receptor,code,template);
 }
