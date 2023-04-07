7fb9dd031d23bf4f2414b6f56bf32405bbd67e12
Reza Dezhhoot <rdezhhoot@gmail.com>
2023-04-08 01:26:18 +0330

    fix bug

--- app/Modules/Auth/Services/SmsService.js
+++ app/Modules/Auth/Services/SmsService.js
@@ -2,6 +2,6 @@ const {send} = require("./SMS/KavehNegarService");
 module.exports.send = async ( receptor , code , template = 'verification') => {
     if (process.env.MODE === 'test')
         return 200;
-    
+
     return await send(receptor,code,template);
 }
--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const {connect: connect1} = require('mongoose')
 const connectDB = async () => {
     try {
-        const conn = await connect1(process.env.mongo_uri);
+        const conn = await connect1(process.env.MONGO_URI);
     } catch (err) {
         console.log(err);
         process.exit(1);
