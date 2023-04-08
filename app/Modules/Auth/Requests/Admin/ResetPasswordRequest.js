49dbd6f5ab5a389506b0e4578fb24114001d20b7
Reza Dezhhoot <rdezhhoot@gmail.com>
2023-04-09 03:06:54 +0330

    refactor code

--- app/Modules/Auth/Requests/Admin/ResetPasswordRequest.js
+++ app/Modules/Auth/Requests/Admin/ResetPasswordRequest.js
@@ -1,6 +1,7 @@
 const Yup = require('yup');
+const {validPhone} = require("../../../../Base/Constants/Regex");
 const schema = Yup.object().shape({
-    phone: Yup.string().matches('^(\\98?)?{?(0?9[0-9]{9,9}}?)$').required('شماره همراه الزامی می باشد'),
+    phone: Yup.string().matches(validPhone).required('شماره همراه الزامی می باشد'),
     code: Yup.number().required('کد تایید الزامی می باشد'),
     password: Yup.string().min(6,'حداقل طول برای رمز عبور 6 حرف می باشد').max(240).required('رمز عبور الزامی می باشد'),
     floatingConfirmation: Yup.string().required().oneOf([Yup.ref('password'),null],"رمز عبور وارد شده معتبر نمی باشد"),
