3fc926e0e887bd50e8b14229ac033b777ebbb88e
Reza Dezhhoot <rdezhhoot@gmail.com>
2023-04-09 02:58:04 +0330

    add sms pattern

--- app/Modules/Auth/Services/SmsService.js
+++ app/Modules/Auth/Services/SmsService.js
@@ -1,5 +1,5 @@
 const {send} = require("./SMS/KavehNegarService");
-module.exports.send = async ( receptor , code , template = 'verification') => {
+module.exports.send = async ( receptor , code , template = 'pirouzverification') => {
     if (process.env.MODE === 'test' || process.env.MODE === 'development')
         return 200;
 
