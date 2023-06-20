a0d459a699ecb0944a70233c826e3c71b0887e3c
Reza Dezhhoot <rdezhhoot@gmail.com>
2023-06-20 03:45:08 +0330

    fix redis buf

--- app/Libraries/Redis.js
+++ app/Libraries/Redis.js
@@ -1,7 +1,6 @@
 const redis = require('redis');
 const client = redis.createClient({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT
+    url: process.env.REDIS_URL
 })
 
 module.exports = client;
